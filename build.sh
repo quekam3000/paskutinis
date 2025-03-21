651cf3226822dbaff45a32534d1c661435565533
quekam3000 <quekam93@gmail.com>
2025-03-22 02:35:48 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Įdiegti yt-dlp
-apt-get update && apt-get install -y \
-    python3 \
-    python3-pip \
-    && pip3 install yt-dlp
+# Įdiegti yt-dlp naudojant pip
+pip install yt-dlp
 
 # Įdiegti npm priklausomybes
 npm install
